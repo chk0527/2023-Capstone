5075c226834cbe87d8560516a99253f323c82732
HyeonKyuChoi <71831720+chk0527@users.noreply.github.com>
2023-03-15 23:34:45 +0900

    youtube url입력받아서 영상 재생

--- React-Native/sample/App.js
+++ React-Native/sample/App.js
@@ -1,34 +1,74 @@
-import React, { useState, useCallback, useRef } from "react";
-import { Button, View, Alert } from "react-native";
+import React, { Component } from "react";
+import { Button, View, Alert,Text,TextInput,StyleSheet } from "react-native";
 import YoutubePlayer from "react-native-youtube-iframe";
-//위 모듈 설치해서 사용 하기! - 추후 다른 모듈 실행해볼것
-//react-native-webview를 먼저 설치하는것을 권장한다고 함
 
-export default function App() {
-  const [playing, setPlaying] = useState(false);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false,
+    };
+  }
+  state = {
+    text: '',
+    inputText: ''
+  }
 
-  const onStateChange = useCallback((state) => {
+ 
+  submitBtn = () => {
+    this.setState({text: this.state.inputText});
+  }
+
+  onStateChange = (state) => {
     if (state === "ended") {
-      setPlaying(false);
+      this.setState({ playing: false });
       Alert.alert("video has finished playing!");
     }
-  }, []);
+  };
 
-  const togglePlaying = useCallback(() => {
-    setPlaying((prev) => !prev);
-  }, []);
-  
+  togglePlaying = () => {
+    this.setState((prevState) => ({ playing: !prevState.playing }));
+  };
 
+  render() {
+    const { playing } = this.state;
 
-  return (
-    <View>
-      <YoutubePlayer
-        height={300}
-        play={playing}
-        videoId={"dSy0mvQDlug"}
-        onChangeState={onStateChange}
-      />
-      <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
-    </View>
-  );
+    return (
+      <View>
+        <YoutubePlayer
+          height={300}
+          play={playing}
+          videoId={this.state.text} //nNyS4srTrn4, dSy0mvQDlug  --youtube id 예시
+          onChangeState={this.onStateChange}
+        />
+        <Button
+          title={playing ? "pause" : "play"}
+          onPress={this.togglePlaying}
+        />
+        <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => {this.setState({inputText: text})}}
+            placeholder="아무거나 입력해주세요."
+          />
+          <Button title="제출" onPress = {this.submitBtn} />
+          <Text style = {styles.showText}>{this.state.text}</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    marginTop: 20,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    height: 40,
+    borderRadius: 10,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  showText: {
+    marginTop: 10,
+    fontSize: 25,
+  }
+})
